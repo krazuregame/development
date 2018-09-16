128d628b2e5b7aa6ea4eaed1232fb0fe6354d516
krazuregame <42531808+krazuregame@users.noreply.github.com>
2018-09-16 17:33:38 +0900

    Update cosmos.py

--- sample/ROSE/examples/cosmos.py
+++ sample/ROSE/examples/cosmos.py
@@ -3,7 +3,7 @@ This driver does not do any action.
 """
 from rose.common import obstacles, actions  # NOQA
 
-driver_name = "CosmosWillBeGooDChangeRead"
+driver_name = "CosmosWillBeGooDChangeRead11"
 
 
 def drive(world):
