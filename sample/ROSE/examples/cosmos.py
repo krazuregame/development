230cc9e7bd1ede139e6f472bf109cab976705784
inhye park <cosmos0703@gmail.com>
2018-09-16 08:35:26 +0000

    Jenkins File Changed

--- sample/ROSE/examples/cosmos.py
+++ sample/ROSE/examples/cosmos.py
@@ -3,7 +3,7 @@ This driver does not do any action.
 """
 from rose.common import obstacles, actions  # NOQA
 
-driver_name = "CosmosWillBeGooDChangeRead112"
+driver_name = "CosmosWillBeGooDChangeRead112i"
 
 
 def drive(world):
