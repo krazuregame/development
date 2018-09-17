2301525111bacabf2159ba4080c912462de93db3
krazuregame <42531808+krazuregame@users.noreply.github.com>
2018-09-17 11:03:58 +0900

    Update deploy.sh

--- sample/deploy.sh
+++ sample/deploy.sh
@@ -1,11 +1,10 @@
+sudo pkill python;
 git clone https://github.com/krazuregame/development.git;
 cd development/sample/ROSE/;
-sudo apt install python-pip;
+sudo apt install python-pip -y;
 pip install --user pipenv;
 source ~/.bashrc;
 pip install --upgrade pip;
 pipenv install;
 pipenv shell;
-./rose-server &;
-./rose-client ./examples/cosmos.py &;
-./rose-client examples/random-driver.py&;
+./rose-server & ./rose-client ./examples/cosmos.py & ./rose-client examples/random-driver.py &
