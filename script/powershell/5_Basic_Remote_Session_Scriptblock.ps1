23bec101a4aded218182edae0cb14719f0908a32
MoonSun Lee <moonsun4249@gmail.com>
2018-09-18 17:54:32 +0900

    Add files via upload

---
+++ script/powershell/5_Basic_Remote_Session_Scriptblock.ps1
@@ -0,0 +1,33 @@
+﻿$username = '*******'
+$userpw = '********'
+$secureuserpw = $userpw | ConvertTo-SecureString -AsPlainText -Force
+$oscred = New-Object pscredential ($username, $secureuserpw)
+
+$Serverlist = Get-Content -Path C:\Users\azureadmin\Desktop\Servername.txt
+Foreach ($Servername in $Serverlist) {
+    Start-Job -Name $ServerName -ScriptBlock { param($ServerName, $oscred)
+
+if($servername -imatch "win")
+{
+    Invoke-Command –ComputerName $ServerName -Credential $oscred -ScriptBlock { Get-Process }
+}
+
+if($servername -imatch "lin")
+{
+    $o = New-PSSessionOption -SkipCACheck -SkipRevocationCheck -SkipCNCheck
+    Invoke-Command -ComputerName $ServerName -Authentication Basic -SessionOption $o -Credential $oscred -ScriptBlock { Get-Process }
+}
+
+
+    } -ArgumentList $Servername, $oscred
+
+}
+
+
+
+<#Servername.txt
+Linux-01
+Linux-02
+Windows-01
+Windows-02
+#>
