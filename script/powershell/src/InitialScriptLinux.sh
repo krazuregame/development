64cd5c86f83bdcae9c7caa0bb1d50d41e52c8175
MoonSun Lee <moonsun4249@gmail.com>
2018-10-19 17:19:17 +0900

    Update InitialScriptLinux.sh

--- script/powershell/src/InitialScriptLinux.sh
+++ script/powershell/src/InitialScriptLinux.sh
@@ -9,35 +9,24 @@ sudo systemctl stop firewalld.service
 
 #Mount Data Disk
 #(echo n; echo p; echo 1; echo ""; echo ""; echo w) | sudo fdisk /dev/sdc
-
 #sudo mkfs -t ext4 /dev/sdc1
-
 #sleep 5
-
 #sudo mkdir /data
-
 #sudo mount /dev/sdc1 /data
-
 #sleep 2
-
 #echo "/dev/sdc   /data ext4 defaults,noatime 1    1" >> /etc/fstab
 
 
 #Add MS Package repository
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
-
 sudo sh -c 'echo -e "[azure-cli]\nname=Azure CLI\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
 
 # Register the Microsoft RedHat repository
 curl https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
 
-
 # Install PowerShell
 sudo yum install -y powershell
-
-# Start PowerShell
-# pwsh
 
 # Install PSRP
 sudo yum -y install omi-psrp-server
@@ -46,10 +35,3 @@ sudo yum -y install omi-psrp-server
 sudo sed -i "s/httpport=0/httpport=0,5985/g" /etc/opt/omi/conf/omiserver.conf
 sudo /opt/omi/bin/service_control restart
 
-# Install az cli
-sudo yum -y install azure-cli
-
-
-# sudo yum -y update
-
-exit 0
